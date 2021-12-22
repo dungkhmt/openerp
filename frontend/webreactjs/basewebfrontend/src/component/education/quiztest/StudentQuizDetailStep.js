e8601926786f94649045edfc74226d5e4a8f9a8e
dungpq <dungkhmt@gmail.com>
2021-12-22 11:03:17 +0700

    fix bug screen security quiz test for teacher

--- frontend/webreactjs/basewebfrontend/src/component/education/quiztest/StudentQuizDetailStep.js
+++ frontend/webreactjs/basewebfrontend/src/component/education/quiztest/StudentQuizDetailStep.js
@@ -1,14 +1,12 @@
-import { useState } from "@hookstate/core";
-import Card from "@material-ui/core/Card";
+import { Card, Button } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import Snackbar from "@material-ui/core/Snackbar";
 import { makeStyles } from "@material-ui/core/styles";
 import Alert from "@material-ui/lab/Alert";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { request } from "../../../api";
 import Quiz from "./Quiz";
-import StudentQuizDetailStep from "./StudentQuizDetailStep";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,21 +23,32 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function StudentQuizDetail() {
+export default function StudentQuizDetailStep() {
   const history = useHistory();
   const testQuizId = history.location.state?.testId;
   const classes = useStyles();
 
   //
-  const [questions, setQuestions] = React.useState([]);
-  const [requestSuccessfully, setRequestSuccessfully] = React.useState(false);
-  const [requestFailed, setRequestFailed] = React.useState(false);
-  const [messageRequest, setMessageRequest] = React.useState(false);
-  const [quizGroupTestDetail, setQuizGroupTestDetail] = React.useState({});
+  const [questions, setQuestions] = useState([]);
+  const [requestSuccessfully, setRequestSuccessfully] = useState(false);
+  const [requestFailed, setRequestFailed] = useState(false);
+  const [messageRequest, setMessageRequest] = useState(false);
+  const [quizGroupTestDetail, setQuizGroupTestDetail] = useState({});
 
   // Keep track of checking state of all choices of all quiz
   const checkState = useState([]);
-
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [question, setQuestion] = useState(null);
+  function handleNextQuestionClick() {
+    let i = currentIndex;
+    setCurrentIndex(i + 1);
+    setQuestion(questions[i + 1]);
+  }
+  function handlePrevQuestionClick() {
+    let i = currentIndex;
+    setCurrentIndex(i - 1);
+    setQuestion(questions[i]);
+  }
   function getQuestionList() {
     request(
       "get",
@@ -52,6 +61,7 @@ export default function StudentQuizDetail() {
         } = res.data;
 
         setQuestions(listQuestion);
+        setQuestion(listQuestion[currentIndex]);
         setQuizGroupTestDetail(quizGroupTestDetail);
 
         // Restore test result
@@ -119,75 +129,33 @@ export default function StudentQuizDetail() {
     );
   };
 
-  const handleCloseSuccess = () => {
-    setRequestSuccessfully(false);
-  };
-
-  const handleCloseError = () => {
-    setRequestFailed(false);
-  };
-
   useEffect(() => {
     getQuestionList();
   }, []);
 
   return (
-    <div className={classes.root}>
-      <Card style={{ padding: "20px 20px 20px 20px" }}>
-        <Snackbar
-          open={requestSuccessfully}
-          autoHideDuration={2000}
-          onClose={handleCloseSuccess}
-        >
-          <Alert variant="filled" severity="success">
-            {messageRequest}
-          </Alert>
-        </Snackbar>
-        <Snackbar
-          open={requestFailed}
-          autoHideDuration={8000}
-          onClose={handleCloseError}
-        >
-          <Alert variant="filled" severity="error">
-            {messageRequest}
-          </Alert>
-        </Snackbar>
-        <div style={{ padding: "0px 20px 20px 30px" }}>
-          <div style={{ justifyContent: "space-between", display: "flex" }}>
-            <h3>Quiz test: {quizGroupTestDetail.testName}</h3>
-            <h3>Môn: {quizGroupTestDetail.courseName}</h3>
-          </div>
-          <h4>Bắt đầu: {quizGroupTestDetail.scheduleDatetime}</h4>
-          <h4>Thời gian: {quizGroupTestDetail.duration} phút</h4>
-        </div>
-
-        {/* Quiz */}
-        <Grid container spacing={3}>
-          {quizGroupTestDetail.quizGroupId ? (
-            questions != null ? (
-              questions.map((question, idx) => (
-                <Quiz
-                  key={question.questionId}
-                  question={question}
-                  choseAnswers={checkState[idx]}
-                  order={idx}
-                  onSave={onSave}
-                />
-              ))
-            ) : (
-              <p style={{ justifyContent: "center" }}>
-                {" "}
-                Chưa có câu hỏi cho mã đề này
-              </p>
-            )
-          ) : (
-            <p style={{ justifyContent: "center" }}>
-              {" "}
-              Chưa phát đề cho sinh viên{" "}
-            </p>
-          )}
-        </Grid>
-      </Card>
+    <div>
+      {currentIndex > 0 ? (
+        <Button onClick={handleNextQuestionClick}>Next</Button>
+      ) : (
+        ""
+      )}
+      {currentIndex < questions.length ? (
+        <Button onClick={handlePrevQuestionClick}>Prev</Button>
+      ) : (
+        ""
+      )}
+      {question && checkState[currentIndex] ? (
+        <Quiz
+          key={question.questionId}
+          question={question}
+          choseAnswers={checkState[currentIndex]}
+          order={currentIndex}
+          onSave={onSave}
+        />
+      ) : (
+        ""
+      )}
     </div>
   );
 }
