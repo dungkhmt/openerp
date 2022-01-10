519243886cd3af6056248e67778dff33e95301ed
dungpq <dungkhmt@gmail.com>
2022-01-10 23:54:08 +0700

    update

--- frontend/webreactjs/basewebfrontend/src/component/education/programmingcontestFE/Test.js
+++ frontend/webreactjs/basewebfrontend/src/component/education/programmingcontestFE/Test.js
@@ -1,22 +1,25 @@
-import SplitPane  from "react-split-pane";
+import SplitPane from "react-split-pane";
 import Pane from "react-split-pane/lib/Pane";
-import {ScrollBox} from "react-scroll-box";
+import { ScrollBox } from "react-scroll-box";
 import Typography from "@material-ui/core/Typography";
-import {Divider} from "@material-ui/core";
-import {Markup} from "interweave";
+import { Divider } from "@material-ui/core";
+import { Markup } from "interweave";
 import * as React from "react";
 import CodeMirror from "@uiw/react-codemirror";
-import {getExtension} from "./lib";
+import { getExtension } from "./lib";
 
-export function Test(){
-  return(
+export function Test() {
+  return (
     <div>
-      <SplitPane split="vertical" allowResize={true} >
-        <Pane initialSize="200px">You can use a Pane component</Pane>
-        <div>or you can use a plain old div</div>
-        <Pane initialSize="70%" minSize="60%"  >Using a Pane allows you to specify any constraints
-          directly</Pane>
-      </SplitPane>
+      {
+        <SplitPane split="vertical" allowResize={true}>
+          <Pane initialSize="200px">You can use a Pane component</Pane>
+          <div>or you can use a plain old div</div>
+          <Pane initialSize="70%" minSize="60%">
+            Using a Pane allows you to specify any constraints directly
+          </Pane>
+        </SplitPane>
+      }
     </div>
   );
 }
