4d4e5c3c154554505b27df3fe462cd67bac33dd3
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2022-05-04 19:04:27 +0700

    [Bug] fix bug: app stops when unable connect to DOCKER_SERVER_HOST in module Programming Contest

--- backend/springboot/baseweb/src/main/java/com/hust/baseweb/applications/programmingcontest/docker/DockerClientBase.java
+++ backend/springboot/baseweb/src/main/java/com/hust/baseweb/applications/programmingcontest/docker/DockerClientBase.java
@@ -4,6 +4,9 @@ import com.hust.baseweb.applications.programmingcontest.constants.Constants;
 import com.hust.baseweb.applications.programmingcontest.utils.ComputerLanguage;
 import com.spotify.docker.client.DefaultDockerClient;
 import com.spotify.docker.client.DockerClient;
+import com.spotify.docker.client.DockerClient.ExecCreateParam;
+import com.spotify.docker.client.DockerClient.ListContainersParam;
+import com.spotify.docker.client.DockerClient.ListImagesParam;
 import com.spotify.docker.client.LogStream;
 import com.spotify.docker.client.exceptions.DockerException;
 import com.spotify.docker.client.messages.*;
@@ -11,7 +14,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.spotify.docker.client.DockerClient.*;
 
 import java.io.IOException;
 import java.net.URI;
@@ -20,13 +22,13 @@ import java.util.*;
 @Configuration
 @Slf4j
 public class DockerClientBase {
+
     @Value("${DOCKER_SERVER_HOST}")
     private String DOCKER_SERVER_HOST;
 
     private static DockerClient dockerClient;
 
-    private static final HashMap<String , String> m = new HashMap<>();
-
+    private static final HashMap<String, String> m = new HashMap<>();
 
     public DockerClientBase() {
     }
@@ -34,153 +36,159 @@ public class DockerClientBase {
     @Bean
     public void initDockerClientBase() throws Exception {
         dockerClient = DefaultDockerClient.builder()
-                .uri(URI.create(DOCKER_SERVER_HOST))
-                .connectionPoolSize(100)
-                .build();
+                                          .uri(URI.create(DOCKER_SERVER_HOST))
+                                          .connectionPoolSize(100)
+                                          .build();
         try {
-            log.info("ping {}" , dockerClient.ping());
+            log.info("ping {}", dockerClient.ping());
             loadNotExistedImage();
             containerExist();
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
 //            System.exit(0);
-            throw new Exception(e.getMessage());
+//            throw new Exception(e.getMessage());
         }
     }
 
     public void containerExist() throws DockerException, InterruptedException {
         List<Container> listContainerStart = dockerClient.listContainers(
-                ListContainersParam.withStatusRunning(),
-                ListContainersParam.filter("label", "names=leetcode")
+            ListContainersParam.withStatusRunning(),
+            ListContainersParam.filter("label", "names=leetcode")
         );
-        for(Container container : listContainerStart){
+
+        for (Container container : listContainerStart) {
             m.put(Objects.requireNonNull(container.names()).get(0), container.id());
         }
+
         List<Container> listContainersStop = dockerClient.listContainers(
-                ListContainersParam.withStatusExited(),
-                ListContainersParam.filter("label", "names=leetcode"));
-        for(Container container : listContainersStop){
+            ListContainersParam.withStatusExited(),
+            ListContainersParam.filter("label", "names=leetcode"));
+
+        for (Container container : listContainersStop) {
             m.put(Objects.requireNonNull(container.names()).get(0), container.id());
             dockerClient.startContainer(container.id());
         }
+
         List<Container> listContainersCreated = dockerClient.listContainers(
-                ListContainersParam.withStatusCreated(),
-                ListContainersParam.filter("label", "names=leetcode"));
-        for(Container container : listContainersCreated){
+            ListContainersParam.withStatusCreated(),
+            ListContainersParam.filter("label", "names=leetcode"));
+
+        for (Container container : listContainersCreated) {
             m.put(Objects.requireNonNull(container.names()).get(0), container.id());
             dockerClient.startContainer(container.id());
         }
+
         log.info("{}", m);
-
-
     }
 
 
     public void loadNotExistedImage() throws DockerException, InterruptedException {
-
         List<Image> list = dockerClient.listImages(ListImagesParam.allImages());
         Set<String> imagesSet = new HashSet<>();
-        for(Image image : list){
+
+        for (Image image : list) {
             imagesSet.add(Objects.requireNonNull(image.repoTags()).get(0));
         }
-        for (Constants.DockerImage i :Constants.DockerImage.values()){
-            if(!imagesSet.contains(i.getValue())){
+
+        for (Constants.DockerImage i : Constants.DockerImage.values()) {
+            if (!imagesSet.contains(i.getValue())) {
                 dockerClient.pull(i.getValue());
             }
         }
 
         List<Container> listContainer = dockerClient.listContainers(
-                ListContainersParam.allContainers(),
-                ListContainersParam.filter("label", "names=leetcode")
+            ListContainersParam.allContainers(),
+            ListContainersParam.filter("label", "names=leetcode")
         );
 
         log.info("list container {}", listContainer);
         Set<String> containerSet = new HashSet<>();
-        for (Container container : listContainer){
+        for (Container container : listContainer) {
             containerSet.add(Objects.requireNonNull(container.names()).get(0));
         }
+
         log.info("containerSet {}", containerSet);
-        for (Constants.DockerContainer dockerContainer : Constants.DockerContainer.values()){
-            Map<String,String> m = new HashMap<>();
+        for (Constants.DockerContainer dockerContainer : Constants.DockerContainer.values()) {
+            Map<String, String> m = new HashMap<>();
             m.put("names", "leetcode");
-            if(!containerSet.contains(dockerContainer.getValue())){
+
+            if (!containerSet.contains(dockerContainer.getValue())) {
                 ContainerConfig containerConfig;
-                switch (dockerContainer){
+                switch (dockerContainer) {
                     case GCC:
                         containerConfig = ContainerConfig.builder()
-                                .image(Constants.DockerImage.GCC.getValue())
-                                .cmd("sh", "-c", "while :; do sleep 1; done")
-                                .labels(m)
-                                .attachStdout(true)
-                                .workingDir("/workdir")
-                                .attachStdin(true)
-                                .build();
+                                                         .image(Constants.DockerImage.GCC.getValue())
+                                                         .cmd("sh", "-c", "while :; do sleep 1; done")
+                                                         .labels(m)
+                                                         .attachStdout(true)
+                                                         .workingDir("/workdir")
+                                                         .attachStdin(true)
+                                                         .build();
                         dockerClient.createContainer(containerConfig, "gcc");
                         break;
                     case JAVA:
                         containerConfig = ContainerConfig.builder()
-                                .image(Constants.DockerImage.JAVA.getValue())
-                                .cmd("sh", "-c", "while :; do sleep 1; done")
-                                .labels(m)
-                                .attachStdout(true)
-                                .workingDir("/workdir")
-                                .attachStdin(true)
-                                .build();
+                                                         .image(Constants.DockerImage.JAVA.getValue())
+                                                         .cmd("sh", "-c", "while :; do sleep 1; done")
+                                                         .labels(m)
+                                                         .attachStdout(true)
+                                                         .workingDir("/workdir")
+                                                         .attachStdin(true)
+                                                         .build();
                         dockerClient.createContainer(containerConfig, "java");
-
                         break;
                     case PYTHON3:
                         containerConfig = ContainerConfig.builder()
-                                .image(Constants.DockerImage.PYTHON3.getValue())
-                                .cmd("sh", "-c", "while :; do sleep 1; done")
-                                .labels(m)
-                                .attachStdout(true)
-                                .workingDir("/workdir")
-                                .attachStdin(true)
-                                .build();
+                                                         .image(Constants.DockerImage.PYTHON3.getValue())
+                                                         .cmd("sh", "-c", "while :; do sleep 1; done")
+                                                         .labels(m)
+                                                         .attachStdout(true)
+                                                         .workingDir("/workdir")
+                                                         .attachStdin(true)
+                                                         .build();
                         dockerClient.createContainer(containerConfig, "python3");
-
                         break;
                     case GOLANG:
                         containerConfig = ContainerConfig.builder()
-                                .image(Constants.DockerImage.GOLANG.getValue())
-                                .cmd("sh", "-c", "while :; do sleep 1; done")
-                                .labels(m)
-                                .workingDir("/workdir")
-                                .attachStdout(true)
-                                .attachStdin(true)
-                                .build();
+                                                         .image(Constants.DockerImage.GOLANG.getValue())
+                                                         .cmd("sh", "-c", "while :; do sleep 1; done")
+                                                         .labels(m)
+                                                         .workingDir("/workdir")
+                                                         .attachStdout(true)
+                                                         .attachStdin(true)
+                                                         .build();
                         dockerClient.createContainer(containerConfig, "golang");
-
                         break;
                 }
             }
         }
-
     }
 
     public String createGccContainer() throws DockerException, InterruptedException {
-        Map<String,String> m = new HashMap<>();
+        Map<String, String> m = new HashMap<>();
         m.put("names", "leetcode");
         ContainerConfig gccContainerConfig = ContainerConfig.builder()
-                    .image("gcc:8.5-buster")
-                .workingDir("/workdir")
-                .hostname("test1")
-                .cmd("sh", "-c", "while :; do sleep 1; done")
-                .labels(m)
-                .attachStdout(true)
-                .attachStdin(true)
-                .build();
+                                                            .image("gcc:8.5-buster")
+                                                            .workingDir("/workdir")
+                                                            .hostname("test1")
+                                                            .cmd("sh", "-c", "while :; do sleep 1; done")
+                                                            .labels(m)
+                                                            .attachStdout(true)
+                                                            .attachStdin(true)
+                                                            .build();
         ContainerCreation gccCreation = dockerClient.createContainer(gccContainerConfig, "gcc");
         dockerClient.startContainer(gccCreation.id());
         return gccCreation.id();
     }
 
-    public String runExecutable(ComputerLanguage.Languages languages, String dirName) throws DockerException, InterruptedException, IOException {
+    public String runExecutable(
+        ComputerLanguage.Languages languages,
+        String dirName
+    ) throws DockerException, InterruptedException, IOException {
         log.info("runExecutable, dirName = " + dirName + " language = " + languages);
-        String[] runCommand = {"bash", dirName+".sh"};
+        String[] runCommand = {"bash", dirName + ".sh"};
         String containerId;
-        switch (languages){
+        switch (languages) {
             case CPP:
                 containerId = m.get("/gcc");
                 break;
@@ -197,14 +205,26 @@ public class DockerClientBase {
                 log.info("language err");
                 return "err";
         }
-        dockerClient.copyToContainer(new java.io.File("./temp_dir/"+dirName).toPath(), containerId, "/workdir/");
+
+        dockerClient.copyToContainer(new java.io.File("./temp_dir/" + dirName).toPath(), containerId, "/workdir/");
+
         log.info("runExecutable, dirName = " + dirName + " language = " + languages + " copyToContainer OK");
         ExecCreation runExecCreation = dockerClient.execCreate(
-                containerId, runCommand, ExecCreateParam.attachStdout(),
-                ExecCreateParam.attachStderr());
-        log.info("runExecutable, dirName = " + dirName + " language = " + languages + " copyToContainer OK -> runExecCreation OK");
+            containerId, runCommand, ExecCreateParam.attachStdout(),
+            ExecCreateParam.attachStderr());
+
+        log.info("runExecutable, dirName = " +
+                 dirName +
+                 " language = " +
+                 languages +
+                 " copyToContainer OK -> runExecCreation OK");
         LogStream output = dockerClient.execStart(runExecCreation.id());
-        log.info("runExecutable, dirName = " + dirName + " language = " + languages + " copyToContainer OK -> runExecCreation OK -> execStart to get output OK");
+
+        log.info("runExecutable, dirName = " +
+                 dirName +
+                 " language = " +
+                 languages +
+                 " copyToContainer OK -> runExecCreation OK -> execStart to get output OK");
         return output.readFully();
     }
 }
