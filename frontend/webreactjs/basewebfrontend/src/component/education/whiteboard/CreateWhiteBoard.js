880a7c6354b7fd62242bc520cc3a4641db38e468
LeDucLoi193 <dekaito193@gmail.com>
2022-05-19 22:21:30 +0700

    Add whiteboard feature

--- frontend/webreactjs/basewebfrontend/src/component/education/whiteboard/CreateWhiteBoard.js
+++ frontend/webreactjs/basewebfrontend/src/component/education/whiteboard/CreateWhiteBoard.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { request } from "../../../api";
+import { nanoid } from 'nanoid'
 
 export default function CreateWhiteBoard() {
-  return <div>CreateWhiteBoard</div>;
+  const history = useHistory();
+
+  const whiteboardId = nanoid();
+  const onCreateNewWhiteboard = async () => {
+    // create new whiteboard
+    await request("post", "/whiteboards", () => {
+      history.push(`/whiteboard/board/${whiteboardId}`)
+    }, {}, { whiteboardId });
+  }
+
+  return <div>
+    <button type="button" onClick={onCreateNewWhiteboard}>Create a new board</button>
+  </div>;
 }
--- frontend/webreactjs/basewebfrontend/src/component/education/whiteboard/ListWhiteBoard.js
+++ frontend/webreactjs/basewebfrontend/src/component/education/whiteboard/ListWhiteBoard.js
@@ -1,5 +1,42 @@
-import React from "react";
+import MaterialTable from "material-table";
+import React, { useEffect, useState } from "react";
+import { Link, useRouteMatch } from "react-router-dom";
+import { request } from "../programmingcontestFE/Request";
 
 export default function ListWhiteBoard() {
-  return <div>ListWhiteBoard</div>;
+  const { path } = useRouteMatch();
+  const [listWhiteboard, setListWhiteboard] = useState([])
+
+  const columns = [
+    {
+      title: "WhiteboardId",
+      field: "id",
+      render: (rowData) => (
+        <Link to={`${path.replace('/list', '')}/${rowData["id"]}`}>{rowData["id"]}</Link>
+      ),
+    },
+    { title: "Name", field: "name", render: (rowData) => (
+      <p>{rowData["name"] ?? `Whiteboard ${rowData["id"]}`}</p>
+    ) },
+    { title: "Created user", field: "createdUser", render: (rowData) => (
+      <p>{rowData["createdBy"]}</p>
+    )},
+  ];
+
+  useEffect(() => {
+    void (async () => {
+      await request("get", '/whiteboards', (res) => {setListWhiteboard(res.data)}, {}, {});
+    })()
+  }, [])
+
+  return (
+    <div>
+      <MaterialTable
+        title="Whiteboard List"
+        columns={columns}
+        data={listWhiteboard}
+      />
+    </div>
+  )
+  
 }
