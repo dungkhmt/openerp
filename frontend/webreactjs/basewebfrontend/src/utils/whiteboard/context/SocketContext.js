0095bda714db60b6446e4dc2330ff960eaa3d314
LeDucLoi193 <dekaito193@gmail.com>
2022-05-21 23:31:04 +0700

    Modify socket server URL

--- frontend/webreactjs/basewebfrontend/src/utils/whiteboard/context/SocketContext.js
+++ frontend/webreactjs/basewebfrontend/src/utils/whiteboard/context/SocketContext.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { io } from 'socket.io-client'
 
-export const socket = io('https://whiteboard-server-graduation.herokuapp.com/')
+export const socket = io('https://openerp.dailyopt.ai/socket-server/')
 
 export const SocketContext = React.createContext({ socket })
 
