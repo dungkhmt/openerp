ded7f58f427eb9ddefd5d1d0d068d8e9b701310d
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2022-05-24 00:37:45 +0700

    [Experiment] fix bug whiteboard

--- frontend/webreactjs/basewebfrontend/src/utils/whiteboard/context/SocketContext.js
+++ frontend/webreactjs/basewebfrontend/src/utils/whiteboard/context/SocketContext.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { io } from "socket.io-client";
 
-export const socket = io("https://openerp.dailyopt.ai");
+export const socket = io();
 
 export const SocketContext = React.createContext({ socket });
 
