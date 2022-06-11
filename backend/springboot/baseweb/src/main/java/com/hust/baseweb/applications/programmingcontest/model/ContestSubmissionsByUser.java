b5eb17fae5cc2722b2a5eed90f73304449fe8e67
Tien4857 <tiennd@sapo.vn>
2022-06-11 21:39:50 +0700

    feat: add new model for ranking

---
+++ backend/springboot/baseweb/src/main/java/com/hust/baseweb/applications/programmingcontest/model/ContestSubmissionsByUser.java
@@ -0,0 +1,18 @@
+package com.hust.baseweb.applications.programmingcontest.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ContestSubmissionsByUser {
+
+    private HashMap<String, Integer> mapProblemsToPoints;
+    private String userId;
+}
