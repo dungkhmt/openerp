ddb00ccb94cdff51d404485409d3bbd50564aace
LeDucLoi193 <dekaito193@gmail.com>
2022-06-23 00:12:04 +0700

    Fix bug: Student cannot create new whiteboard

--- frontend/webreactjs/basewebfrontend/src/component/education/whiteboard/ListWhiteboardStudentView.js
+++ frontend/webreactjs/basewebfrontend/src/component/education/whiteboard/ListWhiteboardStudentView.js
@@ -2,15 +2,11 @@ import { Button, Tooltip } from '@material-ui/core'
 import MaterialTable from 'material-table'
 import React, { useEffect, useState } from 'react'
 import { Link, useRouteMatch, useParams, useHistory } from 'react-router-dom'
-import AddIcon from '@material-ui/icons/Add'
 import { request } from '../../../api'
-import { nanoid } from 'nanoid'
-import { KEYS, ROLE_STATUS } from '../../../utils/whiteboard/constants'
 
-export default function ListWhiteBoard() {
+export default function ListWhiteBoardStudentView() {
   const { url } = useRouteMatch()
   const { sessionId } = useParams()
-  const history = useHistory()
   const [listWhiteboard, setListWhiteboard] = useState([])
   const columns = [
     {
@@ -36,34 +32,6 @@ export default function ListWhiteBoard() {
       )
     })()
   }, [])
-
-  const onCreateNewWhiteboard = async () => {
-    const whiteboardId = nanoid()
-    // create new whiteboard
-    localStorage.removeItem(KEYS.DRAW_DATA_LOCAL_STORAGE)
-    localStorage.removeItem(KEYS.TOTAL_PAGE)
-    localStorage.removeItem(KEYS.CURRENT_PAGE)
-    await request(
-      'post',
-      '/whiteboards',
-      async () => {
-        await request(
-          'put',
-          `/whiteboards/user/${whiteboardId}`,
-          (res) => {
-            if (res.status === 200) {
-              localStorage.setItem(KEYS.USER_ID, res?.data?.userId)
-            }
-            history.push(`${url}/whiteboard/${whiteboardId}?page=1`)
-          },
-          {},
-          { roleId: ROLE_STATUS.WRITE, statusId: ROLE_STATUS.ACCEPTED },
-        )
-      },
-      {},
-      { whiteboardId, classSessionId: sessionId },
-    )
-  }
 
   return (
     <div>
@@ -92,21 +60,6 @@ export default function ListWhiteBoard() {
         style={{
           fontSize: 14,
         }}
-        actions={[
-          {
-            icon: () => {
-              return (
-                <Tooltip title="Thêm bảng viết" aria-label="Thêm bảng viết" placement="top">
-                  <Button variant="contained" color="primary" onClick={onCreateNewWhiteboard}>
-                    <AddIcon style={{ color: 'white' }} fontSize="default" />
-                    &nbsp;&nbsp;&nbsp;Thêm mới&nbsp;&nbsp;
-                  </Button>
-                </Tooltip>
-              )
-            },
-            isFreeAction: true,
-          },
-        ]}
       />
     </div>
   )
