73168a324a5010d996f431373696d448141138cd
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2022-06-26 11:57:07 +0700

    test: undo previous test

--- backend/springboot/baseweb/src/main/java/com/hust/baseweb/BasewebApplication.java
+++ backend/springboot/baseweb/src/main/java/com/hust/baseweb/BasewebApplication.java
@@ -4,21 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.util.Properties;
-
 @EnableScheduling
 @SpringBootApplication
 public class BasewebApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(BasewebApplication.class, args);
-
-        SpringApplication application = new SpringApplication(BasewebApplication.class);
-        Properties properties = new Properties();
-
-        properties.setProperty("spring.main.banner-mode", "off");
-        properties.setProperty("logging.pattern.console", "");
-        application.setDefaultProperties(properties);
-        application.run(args);
+        SpringApplication.run(BasewebApplication.class, args);
     }
 }
