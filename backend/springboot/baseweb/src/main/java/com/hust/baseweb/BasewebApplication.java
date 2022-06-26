b408ca135d9a31d2fb56b52c203b7913d54e17ae
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2022-06-26 11:20:32 +0700

    test: turn off logging

--- backend/springboot/baseweb/src/main/java/com/hust/baseweb/BasewebApplication.java
+++ backend/springboot/baseweb/src/main/java/com/hust/baseweb/BasewebApplication.java
@@ -1,15 +1,24 @@
 package com.hust.baseweb;
 
-import com.hust.baseweb.applications.cache.UserCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Properties;
 
 @EnableScheduling
 @SpringBootApplication
 public class BasewebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BasewebApplication.class, args);
+//        SpringApplication.run(BasewebApplication.class, args);
+
+        SpringApplication application = new SpringApplication(BasewebApplication.class);
+        Properties properties = new Properties();
+
+        properties.setProperty("spring.main.banner-mode", "off");
+        properties.setProperty("logging.pattern.console", "");
+        application.setDefaultProperties(properties);
+        application.run(args);
     }
 }
