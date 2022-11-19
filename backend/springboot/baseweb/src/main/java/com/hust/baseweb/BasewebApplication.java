59fa28ffbfed792ba16a8e2193bea8a39ad9c4d6
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2022-11-19 13:46:25 +0700

    [Test] check java opts

--- backend/springboot/baseweb/src/main/java/com/hust/baseweb/BasewebApplication.java
+++ backend/springboot/baseweb/src/main/java/com/hust/baseweb/BasewebApplication.java
@@ -4,11 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+
 @EnableScheduling
 @SpringBootApplication
 public class BasewebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BasewebApplication.class, args);
+        int mb = 1000 * 1000;
+        MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
+        long xmx = memoryBean.getHeapMemoryUsage().getMax() / mb;
+        long xms = memoryBean.getHeapMemoryUsage().getInit() / mb;
+        System.out.println("Initial Memory (xms) : " + xms + "mb");
+        System.out.println("Max Memory (xmx) : " + xmx + "mb");
     }
 }
