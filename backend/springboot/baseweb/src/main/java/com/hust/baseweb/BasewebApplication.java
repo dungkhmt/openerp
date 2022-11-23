51012b4c84fcde85e98fdb58671da5f9bd2f1c3c
Tien4857 <tiennd@sapo.vn>
2022-11-23 14:51:07 +0700

    chore: optimize import

--- backend/springboot/baseweb/src/main/java/com/hust/baseweb/BasewebApplication.java
+++ backend/springboot/baseweb/src/main/java/com/hust/baseweb/BasewebApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.lang.management.ManagementFactory;
-import java.lang.management.MemoryMXBean;
-
 @EnableScheduling
 @SpringBootApplication
 public class BasewebApplication {
