bab511ee08132b1ac5561bedddf72fd64993e105
Tien4857 <tiennd@sapo.vn>
2022-12-24 21:27:48 +0700

    feat: update page create problem

--- frontend/webreactjs/basewebfrontend/src/component/education/programmingcontestFE/CreateProblem.js
+++ frontend/webreactjs/basewebfrontend/src/component/education/programmingcontestFE/CreateProblem.js
@@ -1,16 +1,5 @@
 import {makeStyles} from "@material-ui/core";
-import {
-  Box,
-  Card,
-  CardContent,
-  Checkbox,
-  Divider,
-  FormControlLabel,
-  InputAdornment,
-  MenuItem,
-  TextField,
-  Typography
-} from "@mui/material";
+import {Box, Checkbox, FormControlLabel, InputAdornment, MenuItem, TextField, Typography} from "@mui/material";
 import React, {useState} from "react";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import {useDispatch, useSelector} from "react-redux";
@@ -25,6 +14,7 @@ import {errorNoti, successNoti, warningNoti} from "../../../utils/notification";
 import HustCodeEditor from "../../common/HustCodeEditor";
 import {LoadingButton} from "@mui/lab";
 import RichTextEditor from "../../common/editor/RichTextEditor";
+import HustContainerCard from "../../common/HustContainerCard";
 
 const useStyles = makeStyles((theme) => ({
   main: {
@@ -45,7 +35,7 @@ const useStyles = makeStyles((theme) => ({
 
 function CreateProblem() {
   const {t} = useTranslation(
-    "education/programmingcontest/problem"
+    ["education/programmingcontest/problem", "common", "validation"]
   );
   const classes = useStyles();
   const history = useHistory();
@@ -105,11 +95,19 @@ function CreateProblem() {
 
   const validateSubmit = () => {
     if (problemId === "") {
-      errorNoti(t("validateSubmit.missingField", {fieldName: "Problem ID"}), 3000);
+      errorNoti(t("missingField", {ns: "validation", fieldName: t("problemId")}), 3000);
       return false;
     }
     if (problemName === "") {
-      errorNoti(t("validateSubmit.missingField", {fieldName: "Problem Name"}), 3000);
+      errorNoti(t("missingField", {ns: "validation", fieldName: t("problemName")}), 3000);
+      return false;
+    }
+    if (timeLimit <= 0 || timeLimit > 60) {
+      errorNoti(t("numberBetween", {ns: "validation", fieldName: t("timeLimit"), min: 1, max: 60}), 3000);
+      return false;
+    }
+    if (memoryLimit <= 0 || timeLimit > 1024) {
+      errorNoti(t("numberBetween", {ns: "validation", fieldName: t("memoryLimit"), min: 1, max: 1024}), 3000);
       return false;
     }
     if (!statusSuccessful) {
@@ -169,7 +167,7 @@ function CreateProblem() {
           });
         },
       )
-      .catch(() => errorNoti("An error happened", 3000))
+      .catch(() => errorNoti(t("error", {ns: "common"}), 3000))
       .finally(() => setLoading(false));
 
     // request(
@@ -188,108 +186,96 @@ function CreateProblem() {
   }
 
   return (
-    <Box>
-      <Card>
-        <Typography
-          fontWeight="600"
-          variant="h5"
-          component="div"
-          sx={{margin: "12px 0 10px 18px"}}
-          color="#2972bb"
+    <HustContainerCard title={t("createProblem")}>
+      <Box className={classes.main}>
+        <TextField
+          autoFocus={true}
+          required
+          id={"problemId"}
+          label={t("problemId")}
+          placeholder="Problem ID"
+          onChange={(event) => {
+            setProblemID(event.target.value);
+          }}
+        />
+        <TextField
+          required
+          id="problemName"
+          label={t("problemName")}
+          placeholder="Problem Name"
+          onChange={(event) => {
+            setProblemName(event.target.value);
+          }}
+        />
+
+        <TextField
+          required
+          select
+          id="levelId"
+          label={t("level")}
+          value={levelId}
+          onChange={(event) => {
+            setLevelId(event.target.value);
+          }}
         >
-          {t("createProblem")}
+          {defaultLevel.map((item) => (
+            <MenuItem key={item} value={item}>
+              {item}
+            </MenuItem>
+          ))}
+        </TextField>
+
+        <TextField
+          required
+          id="timeLimit"
+          label={t("timeLimit")}
+          placeholder="Time Limit"
+          type="number"
+          value={timeLimit}
+          onChange={(event) => {
+            setTimeLimit(event.target.value);
+          }}
+          InputProps={{endAdornment: <InputAdornment position="end">s</InputAdornment>,}}
+        />
+
+        <TextField
+          required
+          id="memoryLimit"
+          label={t("memoryLimit")}
+          type="number"
+          value={memoryLimit}
+          onChange={(event) => {
+            setMemoryLimit(event.target.value);
+          }}
+          InputProps={{endAdornment: <InputAdornment position="end">MB</InputAdornment>,}}
+        />
+
+        <TextField
+          select
+          id="isPublicProblem"
+          label={t("public", {ns: "common"})}
+          onChange={(event) => {
+            setIsPublic(event.target.value);
+          }}
+          value={isPublic}
+        >
+          <MenuItem key={"true"} value={true}>
+            {t("yes", {ns: "common"})}
+          </MenuItem>
+          <MenuItem key={"false"} value={false}>
+            {t("no", {ns: "common"})}
+          </MenuItem>
+        </TextField>
+      </Box>
+
+      <Box className={classes.description}>
+        <Typography variant="h5" component="div" sx={{marginTop: "12px", marginBottom: "8px"}}>
+          {t("problemDescription")}
         </Typography>
-        <Divider/>
-        <CardContent sx={{padding: "24px"}}>
-          <Box className={classes.main}>
-            <TextField
-              autoFocus={true}
-              required
-              id={t("problemId")}
-              label="Problem ID"
-              placeholder="Problem ID"
-              onChange={(event) => {
-                setProblemID(event.target.value);
-              }}
-            />
-            <TextField
-              required
-              id="problemName"
-              label={t("problemName")}
-              placeholder="Problem Name"
-              onChange={(event) => {
-                setProblemName(event.target.value);
-              }}
-            />
-
-            <TextField
-              required
-              select
-              id="levelId"
-              label={t("level")}
-              value={levelId}
-              onChange={(event) => {
-                setLevelId(event.target.value);
-              }}
-            >
-              {defaultLevel.map((item) => (
-                <MenuItem key={item} value={item}>
-                  {item}
-                </MenuItem>
-              ))}
-            </TextField>
-
-            <TextField
-              required
-              id="timeLimit"
-              label={t("timeLimit")}
-              placeholder="Time Limit"
-              type="number"
-              value={timeLimit}
-              onChange={(event) => {
-                setTimeLimit(event.target.value);
-              }}
-              InputProps={{endAdornment: <InputAdornment position="end">s</InputAdornment>,}}
-            />
-
-            <TextField
-              required
-              id="memoryLimit"
-              label={t("memoryLimit")}
-              type="number"
-              value={memoryLimit}
-              onChange={(event) => {
-                setMemoryLimit(event.target.value);
-              }}
-              InputProps={{endAdornment: <InputAdornment position="end">MB</InputAdornment>,}}
-            />
-
-            <TextField
-              select
-              id="isPublicProblem"
-              label="Public Problem"
-              onChange={(event) => {
-                setIsPublic(event.target.value);
-              }}
-              value={isPublic}
-            >
-              <MenuItem key={"true"} value={true}>
-                {"true"}
-              </MenuItem>
-              <MenuItem key={"false"} value={false}>
-                {"false"}
-              </MenuItem>
-            </TextField>
-          </Box>
-
-          <Box className={classes.description}>
-            <Typography variant="h5" component="div" sx={{marginTop: "12px", marginBottom: "8px"}}>
-              {t("problemDescription")}
-            </Typography>
-            <RichTextEditor content={description} onContentChange={text => setDescription(text)}/>
-            <HustDropzoneArea onChangeAttachment={(files) => handleAttachmentFiles(files)}/>
-          </Box>
-          {/* this function is not implemented yet
+        <RichTextEditor content={description} onContentChange={text => setDescription(text)}/>
+        <HustDropzoneArea onChangeAttachment={(files) => handleAttachmentFiles(files)}/>
+      </Box>
+      {/* this function is not implemented yet
               <Box>
                 <Typography>
                   <h2>{t("problemSuggestion")}</h2>
@@ -301,69 +287,67 @@ function CreateProblem() {
               </Box>
               */}
 
-          <HustCodeEditor
-            title={t("correctSourceCode")}
-            language={languageSolution}
-            onChangeLanguage={(event) => {
-              setLanguageSolution(event.target.value);
-            }}
-            sourceCode={codeSolution}
-            onChangeSourceCode={(code) => {
-              setCodeSolution(code);
-            }}
-          />
-          <LoadingButton
-            variant="contained"
-            loading={loading}
-            onClick={checkCompile}
-            sx={{marginTop: "12px", marginBottom: "6px"}}
-          >
-            Check Solution Compile
-          </LoadingButton>
+      <HustCodeEditor
+        title={t("correctSourceCode")}
+        language={languageSolution}
+        onChangeLanguage={(event) => {
+          setLanguageSolution(event.target.value);
+        }}
+        sourceCode={codeSolution}
+        onChangeSourceCode={(code) => {
+          setCodeSolution(code);
+        }}
+      />
+      <LoadingButton
+        variant="contained"
+        loading={loading}
+        onClick={checkCompile}
+        sx={{marginTop: "12px", marginBottom: "6px"}}
+      >
+        {t("checkSolutionCompile")}
+      </LoadingButton>
 
-          <CompileStatus
-            showCompile={showCompile}
-            statusSuccessful={statusSuccessful}
-            message={compileMessage}
-          />
-          <FormControlLabel
-            label={t("allowSubmittingOutput")}
-            control={
-              <Checkbox
-                checked={allowSubmittingOutput}
-                onChange={() => setAllowSubmittingOutput(!allowSubmittingOutput)}
-              />}
-          />
+      <CompileStatus
+        showCompile={showCompile}
+        statusSuccessful={statusSuccessful}
+        message={compileMessage}
+      />
 
-          {allowSubmittingOutput &&
-            <HustCodeEditor
-              title={t("checkerSourceCode")}
-              language={solutionCheckerLanguage}
-              onChangeLanguage={(event) => {
-                setSolutionCheckerLanguage(event.target.value);
-              }}
-              sourceCode={solutionChecker}
-              onChangeSourceCode={(code) => {
-                setSolutionChecker(code);
-              }}
-              placeholder={t("checkerSourceCodePlaceholder")}
-            />
-          }
+      <FormControlLabel
+        label={t("allowSubmittingOutput")}
+        control={
+          <Checkbox
+            checked={allowSubmittingOutput}
+            onChange={() => setAllowSubmittingOutput(!allowSubmittingOutput)}
+          />}
+      />
 
-          <Box width="100%" sx={{marginTop: "16px"}}>
-            <LoadingButton
-              variant="contained"
-              color="success"
-              loading={loading}
-              onClick={handleSubmit}
-            >
-              Save
-            </LoadingButton>
-          </Box>
-        </CardContent>
+      {allowSubmittingOutput &&
+        <HustCodeEditor
+          title={t("checkerSourceCode")}
+          language={solutionCheckerLanguage}
+          onChangeLanguage={(event) => {
+            setSolutionCheckerLanguage(event.target.value);
+          }}
+          sourceCode={solutionChecker}
+          onChangeSourceCode={(code) => {
+            setSolutionChecker(code);
+          }}
+          placeholder={t("checkerSourceCodePlaceholder")}
+        />
+      }
 
-      </Card>
-    </Box>
+      <Box width="100%" sx={{marginTop: "16px"}}>
+        <LoadingButton
+          variant="contained"
+          color="success"
+          loading={loading}
+          onClick={handleSubmit}
+        >
+          {t("save", {ns: "common"})}
+        </LoadingButton>
+      </Box>
+    </HustContainerCard>
   );
 }
 
