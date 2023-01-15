df35759b4decd9d1d6d584bc1c51ee17c1451eab
NguyenDuy-Tien <tien.nd194857@sis.hust.edu.vn>
2023-01-15 23:10:17 +0700

    fix: problem query

--- backend/springboot/baseweb/src/main/java/com/hust/baseweb/applications/programmingcontest/repo/ProblemRepo.java
+++ backend/springboot/baseweb/src/main/java/com/hust/baseweb/applications/programmingcontest/repo/ProblemRepo.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface ProblemRepo extends JpaRepository<ProblemEntity, String> {
     ProblemEntity findByProblemId(String problemId);
 
-    @Query("SELECT p FROM ProblemEntity p JOIN FETCH p.tags WHERE p.id = (:problemId)")
+    @Query("SELECT p FROM ProblemEntity p JOIN FETCH p.tags WHERE p.problemId = (:problemId)")
     ProblemEntity findByProblemIdWithTagFetched(@Param("problemId") String problemId);    
 
     @Query("select p.problemName from ProblemEntity p")
