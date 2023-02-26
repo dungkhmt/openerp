d02a3567841aed517c4dc22ed34246ce12f6d5f4
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2023-02-26 15:42:33 +0700

    [bug] change: get menu by roles in token instead of db

--- backend/springboot/baseweb/src/main/java/com/hust/baseweb/controller/MenuController.java
+++ backend/springboot/baseweb/src/main/java/com/hust/baseweb/controller/MenuController.java
@@ -2,20 +2,16 @@ package com.hust.baseweb.controller;
 
 import com.hust.baseweb.constant.ApplicationTypeConstant;
 import com.hust.baseweb.entity.Application;
-import com.hust.baseweb.entity.SecurityGroup;
 import com.hust.baseweb.entity.SecurityPermission;
-import com.hust.baseweb.entity.UserLogin;
 import com.hust.baseweb.service.ApplicationService;
-import com.hust.baseweb.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -27,23 +23,26 @@ public class MenuController {
 
     public static final String module = MenuController.class.getName();
     private ApplicationService applicationService;
-    private UserService userService;
 
     @GetMapping("/menu")
-    public ResponseEntity<Set> getMenu(Principal principal) {
-        UserLogin userLogin = userService.findById(principal.getName());
-//        System.out.println(module + "::getMenu, userName = " + principal.getName());
+    public ResponseEntity<Set> getMenu(JwtAuthenticationToken token) {
+        List<SecurityPermission> permissions = token
+            .getAuthorities()
+            .stream()
+            .filter(grantedAuthority -> !grantedAuthority
+                .getAuthority()
+                .startsWith("ROLE_GR")) // remove all composite roles
+            .map(grantedAuthority -> { // convert role to permission
+                String permissionId = grantedAuthority.getAuthority().substring(5); // remove prefix "ROLE_"
+                SecurityPermission permission = new SecurityPermission();
+                permission.setPermissionId(permissionId);
+                return permission;
+            })
+            .collect(Collectors.toList());
 
-        List<SecurityPermission> permissionList = new ArrayList<>();
-        if (userLogin != null) {
-            for (SecurityGroup securityGroup : userLogin.getRoles()) {
-                permissionList.addAll(securityGroup.getPermissions());
-            }
-        }
-//        System.out.println(module + "::getMenu, userName = " + principal.getName() + ", meu.lst = " + permissionList.size());
         return ResponseEntity.ok().body(
             applicationService
-                .getListByPermissionAndType(permissionList, ApplicationTypeConstant.MENU)
+                .getListByPermissionAndType(permissions, ApplicationTypeConstant.MENU)
                 .stream()
                 .map(Application::getApplicationId)
                 .collect(Collectors.toSet())
