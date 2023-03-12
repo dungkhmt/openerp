6fdd04acfb71ce9ca9b8bc71465cf9e17f16cfe1
AnhTuan-AiT <anhtuan0126104@gmail.com>
2023-03-12 19:48:13 +0700

    [refactor] consider to remove

--- frontend/webreactjs/basewebfrontend/src/action/Auth.js
+++ frontend/webreactjs/basewebfrontend/src/action/Auth.js
@@ -1,9 +1,9 @@
-export const LOGIN_FAILURE = "LOGIN_FAILURE";
+// export const LOGIN_FAILURE = "LOGIN_FAILURE";
 
-export const failed = (errorState = false, errorMsg = null) => {
-  return {
-    type: LOGIN_FAILURE,
-    errorState: errorState,
-    errorMsg: errorMsg,
-  };
-};
+// export const failed = (errorState = false, errorMsg = null) => {
+//   return {
+//     type: LOGIN_FAILURE,
+//     errorState: errorState,
+//     errorMsg: errorMsg,
+//   };
+// };
